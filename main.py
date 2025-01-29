fff4632840df2c091dc6914d68e8018862ccf6f8
ochoa117 <williamochoa96@gmail.com>
2025-01-29 10:44:48 -0800

    make group containers for game objects and add asteroid-related code

--- main.py
+++ main.py
@@ -1,21 +1,37 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    asteroids = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
+
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+        
+        updatable.update(dt)
+
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+
+        for object in drawable:
+            object.draw(screen)
+            
         pygame.display.flip()
 
         # limit framerate to 60 FPS
